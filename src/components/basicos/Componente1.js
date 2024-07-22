cfc2c93542fe45e055933d932737c5f30288e20a
Gabriel Rodrigo <gabrielrodrigo252004@gmail.com>
2024-07-22 13:25:57 -0300

    enviando repositorio

---
+++ src/components/basicos/Componente1.js
@@ -0,0 +1,3 @@
+export default function ComponenteBasico() {
+    return 'Ola mundo!'
+}
---
+++ src/components/basicos/ComponenteJSX.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ComponenteJSX() {
+    const mensagem = 'Esta é apenas mais uma pratica , ate o momento so estudei o basico sobre componentes'
+    return (
+        <div>
+            <h2>Sejam Bem Vindos(a)!</h2>
+            <p>{mensagem}</p>
+        </div>
+    )
+}
